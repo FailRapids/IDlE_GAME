106be6de9f960e69396a78373a0170b0ad4dbec4
FailRapids <travisdavis_p@outlook.com>
2018-04-14 15:19:32 -0500

    Dummy TimerManger

---
+++ UTIL/timer.gd
@@ -0,0 +1,24 @@
+extends Node
+
+class timer:
+	func _init(timer,progressBar):
+		pass
+	func update_Bar(delta):
+		pass
+	func _connect(who):
+		pass
+	
+	func _disconect(who):
+		pass
+		
+	func Stop():
+		pass 
+	func Pause():
+		pass
+	func unPause():
+		pass
+	func Start(step,time):
+		pass
+	
+	func kill():
+		pass 
